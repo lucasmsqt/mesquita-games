492a2f79ce4f9c41d3532e35d5f3a67ed69a4d3c
Mesquita <lucacsk3@gmail.com>
2024-08-02 02:46:55 -0300

    feat: adiciona geração de etag

--- api/mesquita-games.ts
+++ api/mesquita-games.ts
@@ -1,9 +1,19 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import { readFileSync } from 'fs'
-import { join } from 'path'
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { createHash } from 'crypto';
 
 export default (req: VercelRequest, res: VercelResponse) => {
     const filePath = join(__dirname, '..', 'mesquita-games.json');
     const fileContents = readFileSync(filePath, 'utf8');
+
+    const etag = createHash('sha1').update(fileContents).digest('hex');
+
+    if (req.headers['if-none-match'] === etag) {
+        res.status(304).end(); 
+        return;
+    }
+
+    res.setHeader('ETag', etag);
     res.status(200).json(JSON.parse(fileContents));
 };
